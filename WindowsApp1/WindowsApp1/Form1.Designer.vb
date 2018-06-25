62e00f1ec9441a1984ff96e27a85d8b6a2421b36
gergopc <gergoesjanka@gmail.com>
2018-06-26 00:59:11 +0300

    initial commit

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dirLabel = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.startBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"JetBrains CLion"})
+        Me.ComboBox1.Location = New System.Drawing.Point(43, 22)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "IDE"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Select Folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 115)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Selected:"
+        '
+        'dirLabel
+        '
+        Me.dirLabel.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.dirLabel.AutoSize = True
+        Me.dirLabel.Location = New System.Drawing.Point(73, 115)
+        Me.dirLabel.Name = "dirLabel"
+        Me.dirLabel.Size = New System.Drawing.Size(39, 13)
+        Me.dirLabel.TabIndex = 4
+        Me.dirLabel.Text = "Label3"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(15, 145)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(340, 121)
+        Me.ListBox1.TabIndex = 5
+        '
+        'startBtn
+        '
+        Me.startBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.startBtn.Location = New System.Drawing.Point(18, 271)
+        Me.startBtn.Name = "startBtn"
+        Me.startBtn.Size = New System.Drawing.Size(75, 23)
+        Me.startBtn.TabIndex = 6
+        Me.startBtn.Text = "Start"
+        Me.startBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 306)
+        Me.Controls.Add(Me.startBtn)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.dirLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Console Runner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dirLabel As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents startBtn As Button
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedItem = ComboBox1.Items(0)
+        LoadData()
+
+    End Sub
+    Private Sub LoadData()
+        dirLabel.Text = My.Settings.ProjectsFolder
+        For Each Project As String In Directory.GetDirectories(My.Settings.ProjectsFolder)
+            ListBox1.Items.Add(Project)
+        Next
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            My.Settings.ProjectsFolder = FolderBrowserDialog1.SelectedPath
+            LoadData()
+        End If
+    End Sub
+    Private Sub StartProject()
+        Dim SelectedProject As String = ListBox1.SelectedItem.ToString
+        Dim strarr() As String = SelectedProject.Split("\"c)
+        Dim ProjectName As String = strarr(strarr.Length - 1)
+        Dim executablePath As New StringBuilder
+        executablePath.Append(SelectedProject)
+        If ComboBox1.SelectedItem = "JetBrains CLion" Then
+            executablePath.Append("\cmake-build-debug\")
+            executablePath.Append(ProjectName)
+            executablePath.Append(".exe")
+            StartProgram(executablePath.ToString, ProjectName)
+        End If
+    End Sub
+    Private Sub StartProgram(path As String, title As String)
+        Process.Start("cmd", "/c title " + title + " && " + path + " && echo. && pause")
+    End Sub
+    Private Sub startBtn_Click(sender As Object, e As EventArgs) Handles startBtn.Click
+        StartProject()
+    End Sub
+End Class
